0799990a223ef045abfb3c4a8a908ceea5d989e7
PerdSonNoeud <84141834+PerdSonNoeud@users.noreply.github.com>
2023-11-10 00:11:23 +0100

    #9 Tower Ideas #12 Enemy Ideas First Tower created, also created parent class for tower and enemy.

--- src/java/model/Monster.java
+++ src/java/model/Monster.java
@@ -1,8 +1,44 @@
 package model;
 
+import geometry.IntCoordinates;
+import geometry.RealCoordinates;
+
 public class Monster {
-    // test
-    void hello(){
-        return;
+    private static int id_num = 0;
+    private final RealCoordinates pos;
+    private final int id;
+    private final int[] health;
+    private final int attack;
+    private final int speed;
+
+
+    public Monster(RealCoordinates pos, int attack, int speed, int health) {
+        this.pos = pos;
+        this.id = id_num;
+        id_num++;
+        this.attack = attack;
+        this.speed = speed;
+        this.health = new int[]{health, health};
+
+    }
+
+    public RealCoordinates getPos() {
+        return pos;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public int[] getHealth() {
+        return health;
     }
 }
