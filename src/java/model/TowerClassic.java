f4ef5ae63462aa91a9d18e4960c05c80d24adf66
PerdSonNoeud <84141834+PerdSonNoeud@users.noreply.github.com>
2023-11-10 00:11:18 +0100

    #9 Tower Ideas #12 Enemy Ideas First Tower created, also created parent class for tower and enemy.

---
+++ src/java/model/TowerClassic.java
@@ -0,0 +1,9 @@
+package model;
+
+import geometry.IntCoordinates;
+
+public class TowerClassic extends Towers {
+    public TowerClassic(IntCoordinates pos) {
+        super(pos, 10, 1, 3, 50);
+    }
+}
