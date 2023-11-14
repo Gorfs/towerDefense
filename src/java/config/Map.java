827e69cc559b42b770730823712a4190c60a8045
archie beales <archiebeales@gmail.com>
2023-11-14 16:53:27 +0100

    changed the strucutre for cells, not finished working on re-structure though

--- src/java/config/Map.java
+++ src/java/config/Map.java
@@ -3,46 +3,82 @@ package config;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.ArrayList;
 
-import static config.Cell.slot;
 
 public class Map {
-    private Cell[][] map = new Cell[10][13];
+    private static Cell[][] map = new Cell[10][13];
+    private static Path initPath;
 
     public void generateMap(String file) {
         map = new Cell[10][13];
         // Get the file that contains the map
         File maze = new File("src/resources/map/" + file + ".txt");
         Scanner myReader;
-        // Try if the file exist
         try {
-            // Read the file maze.txt
             myReader = new Scanner(maze);
             int n = 0;
-            // while there is something to read
             while (myReader.hasNextLine()) {
-                // Get the curent line
                 String line = myReader.nextLine();
-                // Split everything into a String array
                 String[] data = line.split(",");
 
                 // For every 2 string
                 for (int i = 0; i < data.length; i ++) {
                     // create a cell based on what there is inside(NOTHING, DOT, etc.)
                     switch (data[i]) {
-                        case "B" -> map[n][i] = slot(Cell.Content.BASE);
-                        case "P" -> map[n][i] = slot(Cell.Content.PATH);
-                        case "S" -> map[n][i] = slot(Cell.Content.SPAWN);
-                        default -> map[n][i] = slot(Cell.Content.SLOT);
+                        case "B" -> map[n][i] = new Cell(i,n);
+                        case "P" -> {
+                            Path tempCell = new Path(i, n);
+                            map[n][i] = tempCell; 
+                            if(initPath == null){initPath = tempCell;}
+                        } 
+                        case "S" -> map[n][i] = new Slot(i, n);
+                        default -> map[n][i] = new Slot(i, n);
                     }
                 }
                 n++;
             }
+            myReader.close();
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         }
     }
 
+    public void generatePath(){
+        // takes the generated map and linkes all the Paths together to make a linked list
+        Path temp = initPath;
+        Path tempPrev = initPath;
+        while(!temp.isBase()){
+
+        }
+
+    }
+
+
+    public ArrayList<Cell> getSurroundingCoordinates(int x, int y){
+        ArrayList<Cell> result = new ArrayList<Cell>();
+        if (x > 0){
+            result.add(map[x-1][y]);
+            if(y > 0){
+                result.add(map[x-1][y-1]);
+            }if (y < map[0].length - 2){
+                result.add(map[x-1][y + 1]);
+            }
+        }else{
+            result.add(map[x + 1][y]);
+        }
+        if (x < map.length - 2){
+            result.add(map[x+1][y]);
+            if(y > 0){
+                result.add(map[x+1][y-1]);
+            }
+            if(y < map[0].length){
+                result.add(map[x+1][y+1]);
+            }
+        }
+        
+
+    }
     public int getWidth() {
         return map[0].length;
     }
@@ -53,5 +89,8 @@ public class Map {
 
     public Cell getCell(int x, int y) {
         return map[x][y];
+    }
+    public static Cell[][] getMap(){
+        return map;
     }
 }
---
+++ src/java/model/gameState.java
@@ -0,0 +1,17 @@
+package model;
+import config.*;
+
+public class gameState {
+    Cell[][] map;
+    int life;
+
+    public gameState(){
+        map = Map.getMap();
+        life = Map.getLife();
+        // the amount of life the player has left.
+        if (map == null){
+            System.out.println("Game state was loaded before config files, therefore no map was loaded, exiting...");
+            System.exit(1);
+        }
+    }
+}
