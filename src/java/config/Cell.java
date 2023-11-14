c7319d37236eeaafe85ebf4672105a3a96c55d71
archie beales <archiebeales@gmail.com>
2023-11-14 16:53:27 +0100

    changed the strucutre for cells, not finished working on re-structure though

--- src/java/config/Cell.java
+++ src/java/config/Cell.java
@@ -1,27 +1,10 @@
 package config;
 
+public class Cell {
+    int x,y;
 
-public record Cell(Cell.Content content) {
-    public enum Content {BASE, PATH, SPAWN, SLOT};
-
-    // TODO: Once class for towers is done, add a variable that can stock a tower if content = SLOT.
-    // private Tower slot(Tower tower) {}
-
-    public static Cell slot(Content c) {
-        return new Cell(c);
-    }
-
-    /**
-    * @return the content of the cell to print it in the console.
-    * */
-    public String termPrint() {
-        if (content == Content.PATH) return ("[] ");
-        else if (content == Content.SLOT) return ("XX ");
-        else return ("-> ");
-    }
-
-    @Override
-    public String toString() {
-        return (content + "");
+    public Cell(int x, int y){
+        this.x = x;
+        this.y = y;
     }
 }
