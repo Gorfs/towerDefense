5aeab20ceff87509d2fd92275e7379eab0519cf3
archie beales <archiebeales@gmail.com>
2023-11-16 14:43:06 +0100

    added the generate Path mechanic so that every path cell has a " next path " to make monster movement easier

--- src/java/config/Cell.java
+++ src/java/config/Cell.java
@@ -1,10 +1,19 @@
 package config;
 
 public class Cell {
-    int x,y;
+    private int x,y;
 
     public Cell(int x, int y){
         this.x = x;
         this.y = y;
+    }
+    public int getX(){
+        return x;
+    }
+    public int getY(){
+        return y;
+    }
+    public String toString(){
+        return x + " " + y;
     }
 }
