f154f2a2a4604006fff1d3814ec44a2fecb534cb
Christophe <christophejin2004@gmail.Com>
2023-12-18 16:24:46 +0000

    Updated terminal display. HP bar and HP percentage now update with the player's health.

--- src/java/gui/TermGame.java
+++ src/java/gui/TermGame.java
@@ -2,6 +2,7 @@ package gui;
 
 import model.GameState;
 import config.*;
+import model.Player;
 
 public class TermGame {
     private static Cell[][] map;
@@ -13,23 +14,37 @@ public class TermGame {
     }
 
     public static void printMap() {
-        String[] display = new String[16];
+        String[] display = new String[17];
         display[0] = "+--------------------------------------------+-------------------+";
-        // TODO: Change the health bar and percentage once player class is added.
-        display[1] = "|           HP   [########..]   100%         |      Timer :      |";
-        // TODO: Same for the money, the wave and the timer.
-        display[2] = "|     Money : 10$      |      Wave n°2       |       00:10       |";
+        // Update health percentage
+        float healthPercent = (float) 85L
+                / Player.INSTANCE.getHealth()[1] * 100;
+        String HPPercent;
+        if (healthPercent < 100.0f) HPPercent = " " + healthPercent;
+        else HPPercent = "" + healthPercent;
+        // Update health bar
+        StringBuilder HPBar = new StringBuilder();
+        for (int i = 0; i < 10; i++) {
+            if (i * 10 < healthPercent - 5) HPBar.append("#");
+            else HPBar.append(".");
+        }
+        display[1] = String.format("|         HP   [%s]   %s", HPBar, HPPercent) + "%         |      Timer :      |";
+        // TODO: Change the money, the wave and the timer.
+        String money = "|     Money : 10$      ";
+        String wave = "|      Wave n°2       ";
+        String timer = "|       00:10       |";
+        display[2] = money + wave + timer;
         display[3] = "+---+----------------------------------------+-------------------+";
         display[4] = "|///|  A  B  C  D  E  F  G  H  I  J  K  L  M | ";
-        display[15] = display[3];
-
+        display[5] = "+---+----------------------------------------+ ";
+        display[16] = display[3];
 
         for (int x = 0; x < Map.getHeight(); x++) {
-            if (x >= 9) display[x + 5] = ("|" + (x + 1) + " | ");
-            else display[x + 5] = ("| " + (x + 1) + " | ");
+            if (x >= 9) display[x + 6] = ("|" + (x + 1) + " | ");
+            else display[x + 6] = ("| " + (x + 1) + " | ");
             for (int y = 0; y < Map.getWidth(); y++)
-                display[x + 5] = display[x + 5] + Map.getCell(x, y) + " ";
-            display[x + 5] = display[x + 5] + "| ";
+                display[x + 6] = display[x + 6] + Map.getCell(x, y) + " ";
+            display[x + 6] = display[x + 6] + "| ";
         }
 
         for (var line: display) System.out.println(line);
