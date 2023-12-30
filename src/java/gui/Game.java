7035d90f4c91e8e282beda95785a6c90eb518b6e
Archie Beales <archiebeales@gmail.com>
2023-12-30 23:13:36 +0100

    first buttons added, swing application in progress

--- src/java/gui/Game.java
+++ src/java/gui/Game.java
@@ -1,10 +1,19 @@
 package gui;
 
+import javax.swing.*;
+
 public class Game {
     private GamePanel gamePanel;
     private GameWindow gameWindow;
+
     public Game() {
-        gamePanel = new GamePanel();
-        gameWindow = new GameWindow(gamePanel);
+        gameWindow = new GameWindow();
+        JButton btn = new JButton("Click me!");
+        gameWindow.getContentPane().add(btn);
+        gameWindow.setVisible(true);
+    }
+
+    public static void main(String[] args){
+        Game game = new Game();
     }
 }
